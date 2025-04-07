d62456fdde54bfac0861335585f6f7fb4af3bdb6
Dedeepya658 <142709060+Dedeepya658@users.noreply.github.com>
2025-04-08 01:07:33 +0800

    Add files via upload

---
+++ Day14_Sorted_Array/solution.java
@@ -0,0 +1,37 @@
+/*iven an array, arr[] sorted in ascending order and an integer k. Return true if k is present in the array, otherwise, false.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 6], k = 6
+Output: true
+Exlpanation: Since, 6 is present in the array at index 4 (0-based indexing), output is true.
+Input: arr[] = [1, 2, 4, 5, 6], k = 3
+Output: false
+Exlpanation: Since, 3 is not present in the array, output is false.
+Input: arr[] = [2, 3, 5, 6], k = 1
+Output: false
+Constraints:
+1 <= arr.size() <= 106
+1 <= k <= 106
+1 <= arr[i] <= 106 */
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+        // Your code here
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == k)
+                return true;
+            else if (arr[mid] < k)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false;
+    
+
+    }
+}
